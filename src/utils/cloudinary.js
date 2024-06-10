38da5fb8e18401248f7de508248258cbc9cd744f
snehitshh <snehitsh07@gmail.com>
2024-06-10 11:36:00 +0530

    api secret fix lol

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,35 +1,3 @@
-/*import {v2 as cloudinary} from 'cloudinary';
-import { response } from 'express';
-import fs from "fs";            //file system
-          
-/*cloudinary.config({ 
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-  api_key: process.env.CLOUDINARY_API_NAME, 
-  api_secret: process.env.CLOUDINARY_API_SECRET 
-});*/
-/*cloudinary.config({ 
-    cloud_name: 'dwxgymfow', 
-    api_key: '323776971944147', 
-    api_secret: '8QA2rR7mHJfbdXEe-e17k_PUuNo' 
-  });
-
-const uploadOnCloudinary=async(localFilePath)=>{
-    try{
-        if(!localFilePath)  return null
-        //uploading file on cloudinary
-   const response = await  cloudinary.uploader.upload(localFilePath,{
-            resource_type:"auto"
-        })
-        //file has been uploaded succesfully
-        console.log("file is uploaded on cloudinary",response.url);
-        return response;
-    }catch(error){
-        fs.unlinkSync(localFilePath)   //removes the locally saved temporary files which failed to upload
-    return null;    
-    }
-}
-  export {uploadOnCloudinary}  */
-
 
 import dotenv from "dotenv" 
 import { v2 as cloudinary } from "cloudinary";
